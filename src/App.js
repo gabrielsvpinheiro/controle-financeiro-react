a2ecce3a77b83ca64098937122b9a99ac21c17b8
gabrielsvpinheiro <gabriel.silva.pinheiro2@gmail.com>
2022-11-20 17:21:21 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <GlobalStyle />
+    </>
+  );
+};
+
+export default App
